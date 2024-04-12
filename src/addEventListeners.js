0d09d92531880f405c5e7895874015cf9512d44b
Ryan <ryan.irani@gmail.com>
2024-04-12 11:04:08 +0100

    deleted

--- src/addEventListeners.js
+++
@@ -1,6 +0,0 @@
-import { addProject } from "./addProject.js";
-
-export function addEventListeners(){
-    const addBtn = document.getElementById('add-project');
-    addBtn.addEventListener("click", addProject)
-}
--- src/addProject.js
+++
@@ -1,9 +0,0 @@
-import { projects } from './projectConstructor.js';
-
-const projectArea = document.getElementsByClassName('dynamically-created-projects');
-
-export function addProject(){
-   
-
-}
-
