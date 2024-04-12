2c57e26d694535e23582c4aa89ce11ea1f8376cf
Ryan <ryan.irani@gmail.com>
2024-04-12 11:03:03 +0100

    updated imports

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import './styles.css';
-import Project from './projectConstructor.js';
-import { addEventListeners } from './addEventListeners.js';
-import { addProject } from './addProject.js';
-import { projects } from './projectConstructor.js';
-
+import { addEventListeners } from './DOM.js';
+import { addProjectToDOM } from './DOM.js';
+import Project from './projects.js';
+import { projects } from './projects.js';
+import { addProjectNameToHeader } from './DOM.js';
 
 
 addEventListeners();
+addProjectNameToHeader("Home");
 
 const project = new Project('Movies'); 
 project.storeProject();
@@ -16,3 +17,4 @@ console.log(projects);
 const project2 = new Project('Cats'); 
 project2.storeProject();
 console.log(projects);
+
