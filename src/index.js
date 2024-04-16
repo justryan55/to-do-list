c82d6ed85813a3db6cd8ab493b47c0b7462038e5
Ryan <ryan.irani@gmail.com>
2024-04-16 09:43:12 +0100

    Project list and to do list functioning

--- src/index.js
+++ src/index.js
@@ -1,98 +1,105 @@
 import './styles.css';
 
-const addProjectBtn = document.getElementById('add-project');
-const projectInputField = document.getElementById('project-input');
-const projectList = document.querySelector('.dynamically-created-projects');
-const header = document.getElementById("header");
-const toDoSection = document.querySelector("display-items-of-project");
-const toDoInputField = document.getElementById("task-item");
-const projects = [];
+class TodoList {
+    constructor() {
+        this.projects = [];
+        this.currentProjectIndex = null;
+        this.$projectNameInput = document.getElementById('project-input');
+        this.$header = document.getElementById("header");
+        this.$projectList = document.querySelector('.dynamically-created-projects');
+        this.$toDoInputField = document.getElementById("task-item");
+        this.$toDoSection = document.getElementById("display-items-of-project");
+        this.$projectInputToggleButton = document.getElementById('add-project');
+        this.createProject("Home");
+
+        this.bindEvents();
+        this.render();
+    }
+
+    createProject(name) {
+        const project = new Project(name);
+        this.projects.push(project);
+
+        this.currentProjectIndex = this.projects.length - 1;
+
+        this.render();
+    }
+    
+    bindEvents() {
+        this.$projectInputToggleButton.addEventListener("click", () => {
+            this.$projectNameInput.classList.toggle("hidden");
+        });
+
+        this.$projectNameInput.addEventListener("keypress", (event) => {
+            if (event.key === "Enter") {
+                this.createProject(this.$projectNameInput.value);
+                this.$projectNameInput.value = "";
+                this.$projectNameInput.classList.toggle("hidden");
+            }
+        });
+
+        this.$toDoInputField.addEventListener("keypress", (event) => {
+            if (event.key !== "Enter"){
+                return;
+            }
+            
+            if (this.$toDoInputField.value === ""){
+                return;
+            }
+
+            const toDoItemText = this.$toDoInputField.value;
+
+            const project = this.projects[this.currentProjectIndex];
+            project.storedItems.push(toDoItemText);
+            this.$toDoInputField.value = "";
+
+            this.render();
+        });
+
+    }
+
+    render() {
+        const currentProject = this.projects[this.currentProjectIndex];
+
+        if(!currentProject) {
+            return;
+        }
+        
+        this.$projectList.innerHTML = "";
+        this.projects.forEach(project => {
+            const $newProject = document.createElement('button');
+            $newProject.classList.add("new-project-button");
+            $newProject.innerText = project.name;
+
+            if(currentProject === project) {
+                $newProject.classList.add("project-current");
+            }
+
+            $newProject.addEventListener('click', () => {
+                this.currentProjectIndex = this.projects.indexOf(project);
+                this.render();
+            });
+
+            this.$projectList.append($newProject);
+        });
+
+        this.$header.innerHTML = currentProject.name;
+
+        this.$toDoSection.innerHTML = "";
+        currentProject.storedItems.forEach(item => {
+            const $toDoItemElement = document.createElement("li");
+            $toDoItemElement.classList.add("to-do-item");
+            $toDoItemElement.innerText = item;
+            this.$toDoSection.append($toDoItemElement);
+        });
+    }
+}
 
 class Project{
     constructor(name){
         this.name = name;
         this.storedItems = [];
-        this.toDoInputField = document.getElementById("task-item");
-        this.toDoSection = document.getElementById("display-items-of-project");
-        this.header = document.getElementById("header");
-    }
-
-    renameHeaderToProject(){
-        this.header.innerText = this.name;
-    }
-
-    displayToDoItems(){
-        this.toDoSection.innerHTML = "";
-        this.storedItems.forEach((item) => {
-            const toDoItemElement = document.createElement("li");
-            toDoItemElement.classList.add("to-do-item");
-            toDoItemElement.innerText = item;
-            this.toDoSection.appendChild(toDoItemElement);
-        })
     }
 }
 
-function addEventListeners(){
-    addProjectBtn.addEventListener("click", toggleInputField);
-    projectInputField.addEventListener("keypress", (event) => {
-        if (event.key === "Enter") {
-            createProject(); 
-            projectInputField.classList.toggle("hidden");
-        }
-    });
-}
-
-function toggleInputField(){
-    projectInputField.classList.toggle("hidden");
-    createProject();
-}
-
-function createProject(){
-    const nameOfProject = projectInputField.value;
-    const newProjectObject = new Project(nameOfProject);
-    appendNewProjectToDOM(nameOfProject, newProjectObject);
-    setupProjectEventListeners(newProjectObject);
-}
-
-function appendNewProjectToDOM(projectName, projectObject){
-    if (projectInputField.value === ""){
-        return
-    }
-
-    projects.push(projectObject);
-    const newProjectDiv = document.createElement('button');
-    newProjectDiv.classList.add("new-project-button");
-    newProjectDiv.innerText = projectName;    
-    projectList.insertBefore(newProjectDiv, projectList.firstChild);
-    projectInputField.value = "";
-    callRenameHeaderMethodOfProject(newProjectDiv, projectObject);
-}
-
-function setupProjectEventListeners(newProjectObject){
-    newProjectObject.toDoInputField.addEventListener("keypress", (event) => {
-        if (event.key === "Enter"){
-            if (newProjectObject.toDoInputField.value === ""){
-                return
-            } else {
-                const toDoItem = newProjectObject.toDoInputField.value;
-                newProjectObject.storedItems.push(toDoItem);
-                newProjectObject.toDoInputField.value = "";
-                newProjectObject.displayToDoItems();
-                console.log(newProjectObject.storedItems);    
-            }
-        }
-    })
-}
-
-function callRenameHeaderMethodOfProject(newProjectDiv, newProjectObject){
-    newProjectDiv.addEventListener("click", () => {
-        newProjectObject.renameHeaderToProject();
-        // callstoreItemsInProjectMethodOfProject(newProjectDiv, newProjectObject);
-    })
-}
-
-// function callstoreItemsInProjectMethodOfProject(newProjectDiv, newProjectObject){
-//     newProjectObject.storeItemsInProject();
-// }
-
-addEventListeners();
+const todoList = new TodoList();
